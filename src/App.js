d2ca9ba6bd046bd66578403d4d1d67c03bf84b5d
Amaro Silva <arcamos.j@gmail.com>
2019-10-16 19:38:48 -0300

    add drag

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 import GlobalStyle from './styles/global'
 import Header from './components/Header';
 import Board from './components/Board';
+import { from } from 'rxjs';
 
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
       <GlobalStyle />
-    </>
+    </DndProvider>
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useDrag } from 'react-dnd'
 
 import { Container, Label } from './styles';
 
 export default function Card ({ data }) {
-    return(
-        <Container>
+    const [{ isDragging }, dragRef] = useDrag({
+        item: { type: 'CARD' },
+        collect: monitor => ({
+            isDragging: monitor.isDragging(),
+        }),
+    })
+
+    return (
+        <Container ref={dragRef}>
             <header>
                 {data.labels.map(label => <Label color={label} />)}
             </header>
