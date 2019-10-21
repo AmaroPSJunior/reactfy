421c21e90cbd57bea44dd5c1448183bcdd9572c8
Amaro Silva <arcamos.j@gmail.com>
2019-10-21 11:29:49 -0300

    finished example

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { loadLists } from '../../services/api'
-import List from '../List';
 import { Container } from './styles';
+import BoardContext from './context';
+import produce from 'immer';
+import List from '../List';
 
-const lists = loadLists();
+
+const data = loadLists();
 
 export default function Board() {
+    const [lists, setList] = useState(data);
+
+    function move(fromList, toList, from, to) {
+        setList(produce(lists, draft => {
+            const dragged = draft[fromList].cards[from];
+            draft[fromList].cards.splice(from, 1);
+            draft[toList].cards.splice(to, 0, dragged);
+
+        }))
+    }
+
     return(
-        <Container>
-            {lists.map(list => <List key={list.title} data={list} />)}
-        </Container>
+        <BoardContext.Provider value={{ lists, move}}>
+            <Container>
+                {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
+            </Container>
+        </BoardContext.Provider>
     );
 }
